78bff263ca666fb00b2c75cce38e65cb350b1994
avvicky <63693675+avvicky@users.noreply.github.com>
2023-01-31 12:33:15 +0530

    vercel config

--- app.js
+++ app.js
@@ -133,5 +133,7 @@ if (port == null || port == "") {
 }
 
 app.listen(port, function () {
-  console.log("Server started on port 3000");
+  console.log("Server started on port " + { port });
 });
+
+module.exports = app;
