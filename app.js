647347dc872e15d2da5946f22956fec0fe58070b
avvicky <63693675+avvicky@users.noreply.github.com>
2023-03-18 19:53:05 +0530

    bug fix

--- app.js
+++ app.js
@@ -128,13 +128,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
+var port = process.env.PORT;
+
+if (port == null) {
+  port = 4000;
 }
 
 app.listen(port, function () {
-  console.log("Server started on port " + port);
+  console.log("server started on port 4000");
 });
-
-// module.exports = app;
